9a32b3072edf1a3493718d3d3321a70fc716c11f
Hung Pham <phamnanghung.25@gmail.com>
2021-05-27 14:20:11 +0700

    add logger

--- src/Redux/storeConfigure.js
+++ src/Redux/storeConfigure.js
@@ -2,13 +2,12 @@ import {applyMiddleware, compose, createStore} from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from '../index_Reducer';
 import rootSaga from '../index_Saga';
-import {composeWithDevTools} from 'remote-redux-devtools';
-
+import logger from 'redux-logger';
 const sagaMiddleware = createSagaMiddleware();
 const configureStore = () => {
-  const middleware = [sagaMiddleware];
+  const middleware = [sagaMiddleware, logger];
   const enhancers = [applyMiddleware(...middleware)];
-  const store = createStore(rootReducer, composeWithDevTools(...enhancers));
+  const store = createStore(rootReducer, compose(...enhancers));
   sagaMiddleware.run(rootSaga);
   return store;
 };
