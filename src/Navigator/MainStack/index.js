125b446da73a0d4d332974cfdd3547d03dde2bde
Hung Pham <phamnanghung.25@gmail.com>
2021-05-28 21:13:24 +0700

    fix bug

--- src/Navigator/MainStack/index.js
+++ src/Navigator/MainStack/index.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import Home from '../../Screens/Home'
+import React from 'react';
+import Home from '../../Screens/Home';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 const Stack = createStackNavigator();
 export default function index() {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={Home} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    )
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
