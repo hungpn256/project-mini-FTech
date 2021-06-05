b70b1358dfd2d4f1440c637518ee85830f71b23d
Bảo Hoàng <goodbyethedrak1@gmail.com>
2021-06-05 15:31:37 +0700

    post

--- src/index_Saga.js
+++ src/index_Saga.js
@@ -1,7 +1,8 @@
 import {all} from 'redux-saga/effects';
 import watchLoginSaga from './Screens/Auth/saga';
 import watchChatSaga from './Screens/ChatRoom/saga';
+import watchPostSaga from './Screens/Home/saga';
 function* rootSaga() {
-  yield all([watchLoginSaga(), watchChatSaga()]);
+  yield all([watchLoginSaga(), watchChatSaga(), watchPostSaga()]);
 }
 export default rootSaga;
