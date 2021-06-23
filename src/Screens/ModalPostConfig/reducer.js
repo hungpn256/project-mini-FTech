8f51eaebcae019acc558f865d6bebd63bdf1cc4e
Bảo Hoàng <goodbyethedrak1@gmail.com>
2021-06-23 13:39:05 +0700

    edit post and delete post

--- src/Screens/ModalPostConfig/reducer.js
+++ src/Screens/ModalPostConfig/reducer.js
@@ -8,12 +8,20 @@ const initialState = {
   status: false,
   postId: '',
   confirm: false,
+  content: '',
+  image: '',
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case OPEN_POST_CONFIG:
-      return {...state, status: true, postId: action.payload.postId};
+      return {
+        ...state,
+        status: true,
+        postId: action.payload.postId,
+        content: action.payload.content,
+        image: action.payload.image,
+      };
     case CLOSE_POST_CONFIG:
       return {...state, status: false};
     case OPEN_CONFIRM:
