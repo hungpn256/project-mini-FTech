096428d249a9fd269b9d3d0e80129625218df5e8
Bảo Hoàng <goodbyethedrak1@gmail.com>
2021-06-23 13:41:12 +0700

    edit post, delete post

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import messaging from '@react-native-firebase/messaging';
+import {LogBox} from 'react-native';
+LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
+LogBox.ignoreAllLogs(); //Ignore all log notifications
 // import admin from 'firebase-admin';
 // // Register background handler
 // import serviceAccount from './src/Configs/serviceadmin.json'
