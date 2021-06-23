76299e19b80ec03f30ef51c4b58338ed5764d057
Hung Pham <phamnanghung.25@gmail.com>
2021-06-23 13:32:58 +0700

    login google, empty, sent mes

--- src/Components/Nothing.js
+++ src/Components/Nothing.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Image} from 'react-native';
 
 const Nothing = () => {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <Image
+        style={{width: '100%', height: 300, resizeMode: 'contain'}}
+        source={require('../Assets/empty.png')}
+      />
+    </View>
+  );
 };
 
 export default Nothing;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
   },
 });
